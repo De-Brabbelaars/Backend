511b7a2f0e278bcb8eafa37490360957369a1b86
Geert404 <gijs.wormgoor@gmail.com>
2025-01-16 10:53:33 +0100

    New db credentials

--- src/postgress/db.mjs
+++ src/postgress/db.mjs
@@ -2,7 +2,8 @@ import mysql from 'mysql2/promise';
 
 // Maak een MySQL connectiepool
 const pool = mysql.createPool({
-  user: 'root',
+  user: 'server',
+  password: 'password',
   host: 'localhost',
   port: 3306, // default MySQL port
   database: 'groeneweide',
