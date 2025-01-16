345b67a2f6e6fe068101e9ad73e345052caa7c82
Geert404 <gijs.wormgoor@gmail.com>
2025-01-16 10:31:30 +0100

    Update db.mjs

--- src/postgress/db.mjs
+++ src/postgress/db.mjs
@@ -2,11 +2,11 @@ import mysql from 'mysql2/promise';
 
 // Maak een MySQL connectiepool
 const pool = mysql.createPool({
-  user: 'user',
-  password: 'user',
+  user: 'root',
+  password: '',
   host: 'localhost',
   port: 3306, // default Postgres port
-  database: 'api'
+  database: 'groeneweide'
 });
 
 export default pool;
